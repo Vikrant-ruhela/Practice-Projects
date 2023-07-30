c6b50092e4c3a56639d8c4086f1e1b628be5d398
Vikrant ruhela <vikrantrohilla607@gmail.com>
2023-07-30 22:46:11 +0530

    blogs with localstorage bug fixed

--- Project1/script.js
+++ Project1/script.js
@@ -9,14 +9,10 @@ const blogDescription = document.getElementById("blogDescription")
 const blogText = document.getElementById("blogText")
 
 const blogBtn = document.querySelector(".blogBtn")
+// const readBtn = document.querySelector(".readBtn")
 
 const blogsGrid = document.querySelector(".blogs")
-const blogs = []
-
-function loadData() {
-    const items = localStorage.getItem("blogs")
-    blogsGrid.innerHTML = items
-}
+let blogs = ""
 
 blogBtn.addEventListener('click', (e) => {
     e.preventDefault()
@@ -27,24 +23,49 @@ blogBtn.addEventListener('click', (e) => {
         blogText: blogText.value
     }
 
-    blogs.push(`<div class="card">
+    blogs = `<div class="card">
     <img src=${blog.blogUrl}>
     <h2>${blog.blogTitle}</h2>
     <p>${blog.blogDescription}</p>
     <button class="readBtn">Read</button>
-    </div>`)
+    </div>`
 
-    blogsGrid.innerHTML = blogs
+    if (localStorage.length > 1) {
+        localStorage.setItem(`${blog.blogTitle}`, JSON.stringify(blog))
+        let items = localStorage.getItem("blogs")
+        items += blogs
+        localStorage.setItem("blogs", items)
+        blogsGrid.innerHTML = items
+        popup.style.display = "none"
+        section.style.filter = ""
+        return
+    }
     localStorage.setItem(`${blog.blogTitle}`, JSON.stringify(blog))
     localStorage.setItem("blogs", `${blogs}`)
+    blogsGrid.innerHTML = blogs
     popup.style.display = "none"
     section.style.filter = ""
+
 })
 
-addBtn.addEventListener("click", () => {
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault()
     popup.style.display = "flex"
     section.style.filter = "blur(8px)"
 })
 
-sub.addEventListener("click", () => {
+sub.addEventListener("click", (e) => {
+    e.preventDefault()
+    popup.style.display = "none"
+    section.style.filter = ""
 })
+
+function loadData() {
+    const items = localStorage.getItem("blogs")
+    blogsGrid.innerHTML = items
+}
+
+// readBtn.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     console.log("clicked on read");
+// })
