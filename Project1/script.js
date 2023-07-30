5bda81bdc6c5dd11221d6a44f031569fd25e8eb4
Vikrant ruhela <vikrantrohilla607@gmail.com>
2023-07-30 21:44:22 +0530

    blogs are added and saved in localstorage

--- Project1/script.js
+++ Project1/script.js
@@ -3,7 +3,43 @@ const popup = document.querySelector(".popup")
 const section = document.querySelector("section")
 const sub = document.querySelector(".sub")
 
+const blogUrl = document.getElementById("blogUrl")
+const blogTitle = document.getElementById("blogTitle")
+const blogDescription = document.getElementById("blogDescription")
+const blogText = document.getElementById("blogText")
 
+const blogBtn = document.querySelector(".blogBtn")
+
+const blogsGrid = document.querySelector(".blogs")
+const blogs = []
+
+function loadData() {
+    const items = localStorage.getItem("blogs")
+    blogsGrid.innerHTML = items
+}
+
+blogBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const blog = {
+        blogUrl: blogUrl.value,
+        blogTitle: blogTitle.value,
+        blogDescription: blogDescription.value,
+        blogText: blogText.value
+    }
+
+    blogs.push(`<div class="card">
+    <img src=${blog.blogUrl}>
+    <h2>${blog.blogTitle}</h2>
+    <p>${blog.blogDescription}</p>
+    <button class="readBtn">Read</button>
+    </div>`)
+
+    blogsGrid.innerHTML = blogs
+    localStorage.setItem(`${blog.blogTitle}`, JSON.stringify(blog))
+    localStorage.setItem("blogs", `${blogs}`)
+    popup.style.display = "none"
+    section.style.filter = ""
+})
 
 addBtn.addEventListener("click", () => {
     popup.style.display = "flex"
@@ -11,6 +47,4 @@ addBtn.addEventListener("click", () => {
 })
 
 sub.addEventListener("click", () => {
-    popup.style.display = "none"
-    section.style.filter = ""
 })
