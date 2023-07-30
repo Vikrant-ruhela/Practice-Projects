fe63015b5e5e0565aba956fc99f5c6af6695ece2
Vikrant ruhela <vikrantrohilla607@gmail.com>
2023-07-30 15:25:34 +0530

    handled edge cases

--- Project2/script.js
+++ Project2/script.js
@@ -4,25 +4,29 @@ const cardGrid = document.querySelector(".cardGrid")
 
 
 const getMovie = () => {
-    fetch(`https://www.omdbapi.com/?s=%22dark%22&apikey=fd998d3`).then((res) => {
+    fetch(`https://www.omdbapi.com/?s=%22avenger%22&apikey=fd998d3`).then((res) => {
         return res.json()
     }).then((result) => {
-        const { Search } = result
-        let r = ""
-        console.log(Search[0].Title);
-        for (let index = 0; index < Search.length; index++) {
-            const element = Search[index];
-            console.log(element);
-            r += ` <div class="card">
+        if (result.Response == "True") {
+            const { Search } = result
+            let r = ""
+            for (let index = 0; index < Search.length; index++) {
+                const element = Search[index];
+                console.log(element);
+                r += ` <div class="card">
                         <img src=${element.Poster} alt="img">
                             <h2>${element.Title}</h2>
                         <button>Watch Now</button>
                   </div>`
 
+            }
+            cardGrid.innerHTML = r;
+        } else {
+            cardGrid.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
         }
-        cardGrid.innerHTML = r;
-
-    })
+    }).catch(() => {
+        cardGrid.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+    });
 }
 
 input.addEventListener("change", getMovie); 
